9fc01f31191a5ceb2353610ed26ff7e05ebad1cb
Matej Sajgal <roarbb@gmail.com>
2026-08-17 17:27:43 +0200

    Removed unnecessary types

--- src/firebase/gallery.ts
+++ src/firebase/gallery.ts
@@ -1,21 +1,18 @@
 import { addDoc, collection, getDocs, query, where } from 'firebase/firestore'
-import type { QueryDocumentSnapshot } from 'firebase/firestore'
 import { db } from '@/firebase/config'
 
 const COLLECTION_NAME_GALLERY = 'gallery'
 const doc = collection(db, COLLECTION_NAME_GALLERY)
 
-export const getUserGalleries = async (
-  userId: string,
-): Promise<Array<QueryDocumentSnapshot>> => {
+export const getUserGalleries = async (userId: string) => {
   const q = query(doc, where('userId', '==', userId))
   const querySnapshot = await getDocs(q)
 
   return querySnapshot.docs
 }
 
-export const addGallery = async ( userId: string): Promise<string> => {
+export const addGallery = async (userId: string) => {
   const docRef = await addDoc(doc, { userId })
 
-  return docRef.id;
+  return docRef.id
 }
