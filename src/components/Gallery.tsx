c9e3f1a66ac90e296577c2e146b07fa224b1fe34
Matej Sajgal <roarbb@gmail.com>
2026-09-05 17:28:24 +0200

    Yeah bad idea, revert

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -178,15 +178,11 @@ export function Gallery({
             handleLeftArrowClick={handleLeftArrowClick}
             handleRightArrowClick={handleRightArrowClick}
           >
-            <div className="h-full w-full flex items-center">
-              <button className='h-fit w-fit' onClick={handleRightArrowClick}>
-                <img
-                  className="object-contain"
-                  src={activePhoto.secure_url}
-                  alt="gallery picture"
-                />
-              </button>
-            </div>
+            <img
+              className="h-full w-full object-contain"
+              src={activePhoto.secure_url}
+              alt="gallery picture"
+            />
           </DialogContent>
         </Dialog>
       )}
